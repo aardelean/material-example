27418a863b45bdd0a14563ea26ea3d207846e0d5
aardelean <alex_i_ardelean@yahoo.com>
2016-01-09 17:34:16 +0100

    init commit working material ui

---
+++ client/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Greeting from "./greetings";
+import RaisedEx from "./raised-ex";
+
+ReactDOM.render(
+ <RaisedEx name="World"/>,
+  document.body
+);
